13704dd0658f01ea40ffa793663ef5a48596309c
ldss3sang <ldss3sang@gmail.com>
2019-06-26 16:10:05 -0400

    connected mongodb(3.0,3.1)

--- db.js
+++ db.js
@@ -1,50 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
     {
-        id:324393,
-        title: "Video Nomad",
-        description: "This is awesome video",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangseok",
-            email: "sangseoksoftware@outlook.com"
-        }
-    },
-    {
-        id:434323,
-        title: "Video Fake",
-        description: "This is awesome video",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangseok",
-            email: "sangseoksoftware@outlook.com"
-        }
-    },
-    {
-        id:4243424,
-        title: "Video Good",
-        description: "This is awesome video",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangseok",
-            email: "sangseoksoftware@outlook.com"
-        }
-    },
-    {
-        id:132423,
-        title: "Nomad Awesome",
-        description: "This is awesome video",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangseok",
-            email: "sangseoksoftware@outlook.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) => console.log(`❌  Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
