2b7c596ebcec68856491bee789f641f75d601713
ldss3sang <ldss3sang@gmail.com>
2019-07-01 14:20:11 -0400

    Added video search(#3.12)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -20,14 +20,26 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+// Search
+export const search = async (req, res) => {
   const {
-    query: { term: searchBy }
+    query: { term: searchingBy }
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: {
+        $regex: searchingBy,
+        $options: "i"
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
   res.render("search", {
     pageTitle: "Search",
-    searchBy,
+    searchingBy,
     videos
   });
 };
